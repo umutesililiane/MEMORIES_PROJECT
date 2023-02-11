8c06a4ce75320a4a22c4ed34914887d519b000a6
umutesililiane <91309873+umutesililiane@users.noreply.github.com>
2023-02-11 14:54:50 +0530

    environmental variablr [adding .env file]

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
  
 import postRoutes from './routes/posts.js';
 const app = express ();
+dotenv.config();
 
 app.use(cors());
 
