0d1284e49bcffdb2b4c8f3e0575db65a3c973285
umutesililiane <91309873+umutesililiane@users.noreply.github.com>
2023-02-11 14:37:21 +0530

    creating action constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,5 @@
 
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 //action creators
@@ -8,7 +9,7 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) =>{
     try{
         const {data} = await api.fetchPosts();
-        dispatch({type:'FETCH_ALL', payload:data });
+        dispatch({type:FETCH_ALL, payload:data });
         } catch(error){
             console.log(error.message);
     }
@@ -17,7 +18,7 @@ export const getPosts = () => async (dispatch) =>{
 export const createPost = (post) => async (dispatch) => {
     try {
         const {data} = await api.createPost(post);
-        dispatch ({type: 'CREATE', payload: data});
+        dispatch ({type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -26,7 +27,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const{data} = await api.updatePost (id, post);
-        dispatch({ type: "UPDATE", payload: data }); 
+        dispatch({ type: UPDATE, payload: data }); 
     } catch (error) {
         console.log(error.message);
         
@@ -36,7 +37,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch  ({ type:'DELETE', payload: id });
+        dispatch  ({ type: DELETE, payload: id });
     } catch (error) {
         console.log (error);
         
@@ -48,7 +49,7 @@ export const likePost = (id) =>async (dispatch) => {
         console.log(id);
         const{data} = await api.likePost (id);
 
-        dispatch({ type: "UPDATE", payload: data }); 
+        dispatch({ type: UPDATE, payload: data }); 
     } catch (error) {
         console.log(error)
         
