0d9c225a9fa52f39bb4e968a063ffd8696aebfdf
umutesililiane <91309873+umutesililiane@users.noreply.github.com>
2023-02-27 19:08:34 +0530

    changing layout , adding sign in button and creating roues from sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid, makeStyles, Theme } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts';
-import Posts from './components/posts/posts';
-import Form from './components/form/form';
-
-
-import useStyles from './styles';
+import React from "react";
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
+import Home from "./components/Home/Home";
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-    return (
+import Auth from "./components/Auth/Auth";
+
+
+const App = () => (
+
+    <BrowserRouter>
+
 
         <Container maxwidth="lg">
             <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/auth" exact component={Auth} />
 
-            <Grow in>
-                <Container >
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            </Switch>
+
+
         </Container>
-    );
-}
+    </BrowserRouter>
+);
+
 export default App;
